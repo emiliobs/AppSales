4106358ce2de70e08b8ee730018a5dba48f22dd7
emilio barrera <barrera_emilio@yahoo.es>
2018-10-04 19:25:30 +0100

    Remarks in the ListView

--- Sales/Sales/Models/Products.cs
+++ Sales/Sales/Models/Products.cs
@@ -11,6 +11,7 @@
         
         public string Description { get; set; }
 
+        public string Remarks { get; set; }
         public string ImagePath { get; set; }
 
         public decimal Price { get; set; }
