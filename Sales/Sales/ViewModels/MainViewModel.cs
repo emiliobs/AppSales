2c50ec8e06cb5ea2bfb61c84941ba9cf6eb40c7a
emilio barrera <barrera_emilio@yahoo.es>
2018-10-05 11:46:18 +0100

    AddProductsPage

--- Sales/Sales/ViewModels/MainViewModel.cs
+++ Sales/Sales/ViewModels/MainViewModel.cs
@@ -1,7 +1,13 @@
 ﻿namespace Sales.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using Sales.Models;
+    using Sales.Views;
+    using System;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
     public class MainViewModel
     {
         #region Service
@@ -25,11 +31,14 @@
         #endregion
 
         #region Commands and Methods
-
+        public ICommand AddProductCommand { get => new RelayCommand(GoToAddProduct); } 
         #endregion
 
         #region Methods
-
+        private async void GoToAddProduct()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new AddProductPage());
+        }
         #endregion
     }
 }
